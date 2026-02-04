8cc462a60545ad841b42751d8f8d77eea316c021
Marchello <paskalovmarkus@gmail.com>
2026-02-04 13:21:38 +0200

    The project structure has been created

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import app from "./app.ts"
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const PORT = process.env.PORT 
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
